44a245e46e7808c42ecf86de0de4d0f71f606bea
tami-cp0 <findtamilore>
2025-02-22 10:54:34 +0100

    initiate server

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+import { config } from "dotenv";
+
+config()
+
+const app = express();
+
+const port: number = Number(process.env.PORT) || 3000;
+const host: string = '0.0.0.0';
+const serverURL: string = process.env.BACKEND_URL as string;
+
+const allowedOrigins: string[] = [serverURL, `http://${host}:${port}`];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true,
+}))
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, host, () => {
+    console.log(`Listening on http://${host}:${port}`);
+})
