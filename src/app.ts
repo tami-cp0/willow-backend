52d2205123d5a1c81397aab1ef01b7511fea43ad
tami-cp0 <findtamilore>
2025-02-23 15:11:25 +0100

    import router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from 'cookie-parser';
 import { config } from "dotenv";
+import router from "./routes";
 
 config()
 
@@ -21,6 +22,8 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(router)
 
 
 app.listen(port, host, () => {
