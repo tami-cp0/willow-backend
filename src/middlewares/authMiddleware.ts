973f1e9e60df1805662510a8e3bf60446dd2a930
tami-cp0 <findtamilore>
2025-02-26 01:07:28 +0100

    setup initial auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,53 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { ErrorHandler } from "../utils/errorHandler";
+import { CustomJwtPayload } from "../interfaces";
+import cache from "../utils/cache";
+import { User } from "@prisma/client";
+import prisma from "../app";
+
+async function authMidlleware(req: Request, res: Response, next: NextFunction) {
+    const accessToken: string | undefined = req.cookies?.accessToken;
+    const secret: string = process.env.JWT_SECRET as string;
+
+    if (!accessToken) {
+        return next(new ErrorHandler(401, 'Login required'));
+    }
+
+    try {
+        const decoded: CustomJwtPayload = jwt.verify(accessToken, secret) as CustomJwtPayload;
+
+        if (await cache.isAccessTokenBlacklisted(decoded._id, accessToken)) {
+            return next(new ErrorHandler(401, "Login required"));
+        }
+
+        let user: User | null = await cache.getUser(decoded._id);
+        if (!user) {
+            const user: User | null = await prisma.user.findUnique({
+                where: {
+                    id: decoded.id
+                }
+            });
+            if (!user) return next(new ErrorHandler(404, "Account does not exist: Invalid ID"));
+            if (!user.isVerified) return next(new ErrorHandler(403, 'Account is not verified'));
+
+            await cache.storeUser(user);
+        }
+
+        req.user = user as User;
+
+        return next();
+    } catch (error: any) {
+        if (error.name === 'TokenExpiredError') {
+            const payload: CustomJwtPayload = jwt.decode(req.cookies?.accessToken) as CustomJwtPayload;
+
+            if (payload && payload.reset) {
+                return next(new ErrorHandler(401, 'Password reset link has expired'));
+            }
+        }
+        console.log(error);
+        return next(new ErrorHandler(401, "Login required"));
+    }
+}
+
+export default authMidlleware;
