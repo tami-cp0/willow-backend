2da0b3ca39d8db47cea8ad8a3253dc08f0dcdf07
tami-cp0 <findtamilore>
2025-02-26 01:07:51 +0100

    type interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,22 @@
+import { User } from "@prisma/client";
+import { JwtPayload } from "jsonwebtoken";
+
+export interface CustomJwtPayload extends JwtPayload {
+    id: string;
+    role: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            user: User;
+        }
+    }
+}
+
+
+// export interface IMulterS3File extends Express.Multer.File {
+//     key: string;
+//     location: string;
+//     bucket: string;
+// }
