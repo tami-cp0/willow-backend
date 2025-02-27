bc44ada940304d8067c385896031dd357c33b4e4
tami-cp0 <findtamilore>
2025-02-27 11:43:19 +0100

    add docs route and redirection

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,18 @@
 import { Router, Request, Response, NextFunction } from "express";
+import swaggerUi from "swagger-ui-express";
 import authRouter from "./auth";
 import { CustomError, handleError } from "../utils/errorHandler";
 import userRouter from "./user";
+import swaggerSpec from "../docs/swagger";
 
 const router = Router();
 
 router.use('/api/v1/auth', authRouter);
 router.use('/api/v1/users', userRouter);
+router.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+router.get("/", (req: Request, res: Response) => {
+    res.redirect("/api/v1/docs");
+});
 
 router.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
     handleError(err, res);
