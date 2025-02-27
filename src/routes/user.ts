7b560dba2db59064773eefd46a9f9ace5a2a8470
tami-cp0 <findtamilore>
2025-02-28 00:41:27 +0100

    update route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import userController from "../controllers/user";
-import { minimalRateLimiter, moderateRateLimiter } from "../utils/rateLimiters";
+import { moderateRateLimiter, strictRateLimiter } from "../utils/rateLimiters";
+import authMiddleware from "../middlewares/authMiddleware";
 
 
 const userRouter = Router();
 
 userRouter.route('/register').post(moderateRateLimiter, userController.register);
-
-// sellers
-userRouter.route('/:sellerId').get(minimalRateLimiter, userController.getSeller);
+userRouter.route('/:userId/delete').delete(strictRateLimiter, authMiddleware, userController.delete);
 
 export default userRouter;
