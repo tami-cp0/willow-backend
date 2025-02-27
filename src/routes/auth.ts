1601bdf875879c69653b3dd3fe8c0cc2f3eee465
tami-cp0 <findtamilore>
2025-02-27 17:18:44 +0100

    changed http methods

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,12 +5,12 @@ import { minimalRateLimiter, moderateRateLimiter, strictRateLimiter } from "../u
 
 const authRouter = Router();
 
-authRouter.route('/verify-account').post(moderateRateLimiter, authController.verifyAccount);
+authRouter.route('/verify-account').patch(moderateRateLimiter, authController.verifyAccount);
 authRouter.route('/resend-otp').post(strictRateLimiter, authController.resendOtp);
 authRouter.route('/login').post(moderateRateLimiter, authController.login);
 authRouter.route('/logout').post(moderateRateLimiter, authMiddleware, authController.logout);
 authRouter.route('/refresh-tokens').post(strictRateLimiter, authMiddleware, authController.refreshAccessToken);
 authRouter.route('/forgot-password').post(strictRateLimiter, authController.forgotPassword); // can be used to resend passwod reset link
-authRouter.route('/password-reset').post(moderateRateLimiter, authController.resetPassword);
+authRouter.route('/password-reset').patch(moderateRateLimiter, authController.resetPassword);
 
 export default authRouter
