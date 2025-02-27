2644035671cb3ac810d7e749843e3702a06eb802
tami-cp0 <findtamilore>
2025-02-27 12:49:56 +0100

    fix route bug

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import userController from "../controllers/user";
+import { minimalRateLimiter, moderateRateLimiter } from "../utils/rateLimiters";
 
 
 const userRouter = Router();
 
-userRouter.route('/register').post(userController.register);
-userRouter.route('/verify-account').post(userController.verifyAccount);
+userRouter.route('/register').post(moderateRateLimiter, userController.register);
+
+// sellers
+userRouter.route('/:sellerId').get(minimalRateLimiter, userController.getSeller);
 
 export default userRouter;
