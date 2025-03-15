2c2e050f8716e1c1ffc6e3c7d09b96190ebe70a8
tami-cp0 <findtamilore>
2025-03-15 11:52:12 +0100

    publicize routes

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -5,8 +5,8 @@ import productController from "../controllers/product";
 
 const productRouter = Router();
 
-productRouter.route('/products').get(minimalRateLimiter, authMiddleware, productController.getAllProducts);
-productRouter.route('/products/:productId').get(minimalRateLimiter, authMiddleware, productController.getSingleProduct);
-productRouter.route('/products/:productId/reviews').get(minimalRateLimiter, authMiddleware, productController.getProductReviews);
+productRouter.route('/products').get(minimalRateLimiter, productController.getAllProducts);
+productRouter.route('/products/:productId').get(minimalRateLimiter, productController.getSingleProduct);
+productRouter.route('/products/:productId/reviews').get(minimalRateLimiter, productController.getProductReviews);
 
 export default productRouter;
--- src/routes/seller.ts
+++ src/routes/seller.ts
@@ -7,19 +7,19 @@ import { upload } from "../middlewares/upload";
 const sellerRouter = Router();
 
 
-sellerRouter.route('/sellers/:userId').get(minimalRateLimiter, authMiddleware, sellerController.getSeller);
+sellerRouter.route('/sellers/:userId').get(minimalRateLimiter, sellerController.getSeller);
 sellerRouter.route('/sellers/:userId/update-profile').patch(moderateRateLimiter, authMiddleware, upload.single('avatar'), sellerController.updateProfile);
 
 sellerRouter.route('/sellers/:userId/orders').get(minimalRateLimiter, authMiddleware, sellerController.getOrders); // pagination; optional queries NEW, PROCESSING, SHIPPED, DELIVERED, CANCELLED, REFUNDED
 sellerRouter.route('/sellers/:userId/orders/:orderId').get(minimalRateLimiter, authMiddleware, sellerController.getOrder);
 sellerRouter.route('/sellers/:userId/orders/:orderId/update-status').patch(minimalRateLimiter, authMiddleware, sellerController.updateOrderStatus);
 
-sellerRouter.route('/sellers/:userId/products').get(minimalRateLimiter, authMiddleware, sellerController.getProducts); // pagination; optional queries PENDING, APPROVED, REJECTED
+sellerRouter.route('/sellers/:userId/products').get(minimalRateLimiter, sellerController.getProducts); // pagination; optional queries PENDING, APPROVED, REJECTED
 sellerRouter.route('/sellers/:userId/products').post(strictRateLimiter, authMiddleware, upload.fields([
     { name: 'certificate', maxCount: 1 },
     { name: 'images', maxCount: 5 }
   ]), sellerController.createProduct);
-sellerRouter.route('/sellers/:userId/products/:productId').get(minimalRateLimiter, authMiddleware, sellerController.getProduct);
+sellerRouter.route('/sellers/:userId/products/:productId').get(minimalRateLimiter, sellerController.getProduct);
 sellerRouter.route('/sellers/:userId/products/:productId').delete(moderateRateLimiter, authMiddleware, sellerController.deleteProduct);
 
 sellerRouter.route('/sellers/:userId/conversations').get(minimalRateLimiter, authMiddleware, sellerController.getConversations);
