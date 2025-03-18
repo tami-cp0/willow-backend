4ba34480be81334770a227a45f58c7ec42507b24
tami-cp0 <findtamilore>
2025-03-18 20:39:58 +0100

    customer DTOs

---
+++ src/dtos/customer/deleteCartItem.dto.ts
@@ -0,0 +1,30 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { Request } from 'express';
+import { ErrorHandler } from '../../utils/errorHandler';
+
+class DeleteCartItemDto {
+  @IsNotEmpty({ message: 'Customer user ID is required' })
+  @IsString({ message: 'Customer user ID must be a string' })
+  userId!: string;
+
+  @IsNotEmpty({ message: 'Product ID is required' })
+  @IsString({ message: 'Product ID must be a string' })
+  productId!: string;
+}
+
+async function validateDeleteCartItemDto(req: Request): Promise<void> {
+    if (req.user.id !== req.params.userId) {
+        throw new ErrorHandler(403, 'Access denied');
+      }
+
+  const dtoInstance = plainToInstance(DeleteCartItemDto, req.params);
+  const errors = await validate(dtoInstance);
+  if (errors.length > 0) {
+    const firstErrorMessage = Object.values(errors[0].constraints || {})[0];
+    throw new ErrorHandler(400, firstErrorMessage);
+  }
+}
+
+export default validateDeleteCartItemDto;
