3f6040b687bda85dbc881f4d4df478fcdb30c17e
tami-cp0 <findtamilore>
2025-03-18 23:09:24 +0100

    initial routes

--- src/routes/customer.ts
+++ src/routes/customer.ts
@@ -1,20 +1,25 @@
 import { Router } from "express";
+import { minimalRateLimiter, moderateRateLimiter } from "../utils/rateLimiters";
+import authMiddleware from "../middlewares/authMiddleware";
+import customerController from "../controllers/customer";
+import PaymentController from "../controllers/payment";
 
 
 const customerRouter = Router();
 
-customerRouter.route('/customers/:userId').get();
-customerRouter.route('/customers/:userId/update-profile').post();
+customerRouter.route('/customers/:userId').get(minimalRateLimiter, authMiddleware, customerController.getCustomer);
+customerRouter.route('/customers/:userId/update-profile').post(moderateRateLimiter, authMiddleware, customerController.updateProfile);
 
-// customerRouter.route('/customers/:userId/cart').get();
-// customerRouter.route('/customers/:userId/cart/checkout').post();
-// customerRouter.route('/customers/:userId/cart/:productId').post();
-// customerRouter.route('/customers/:userId/cart/:productId').patch();
-// customerRouter.route('/customers/:userId/cart/:productId').delete();
+customerRouter.route('/customers/:userId/cart').get(minimalRateLimiter, authMiddleware, customerController.getCart);
+customerRouter.route('/customers/:userId/cart/:productId').put(minimalRateLimiter, authMiddleware, customerController.upsertCartItem); // add new or update cart item using upsert
+customerRouter.route('/customers/:userId/cart/:productId').delete();
+customerRouter.route('/customers/:userId/cart/checkout').post(minimalRateLimiter, PaymentController.initializePayment);
+ 
 
-// customerRouter.route('/customers/:userId/liked-products').post();
-// customerRouter.route('/customers/:userId/liked-products').get();
-// customerRouter.route('/customers/:userId/liked-products').delete();
+
+customerRouter.route('/customers/:userId/liked-products').post();
+customerRouter.route('/customers/:userId/liked-products').get();
+customerRouter.route('/customers/:userId/liked-products').delete();
 
 // customerRouter.route('/customers/:userId/last-viewed').post();
 // customerRouter.route('/customers/:userId/last-viewed').get();
