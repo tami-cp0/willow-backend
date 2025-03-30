9eb17e309aaf127878bbd0fe63a6efc05ddb0415
tami-cp0 <findtamilore>
2025-03-30 15:44:18 +0100

    change token extraction and error messages

--- src/middlewares/wsAuthMiddleware.ts
+++ src/middlewares/wsAuthMiddleware.ts
@@ -10,19 +10,19 @@ export default async function wsAuthMiddleware(
   ws: WebSocket, 
   req: Request
 ): Promise<any> {
-  const token = req.query.token as string; // use cookies later
+  const accessToken: string | undefined = req.cookies?.accessToken;
   
-  if (!token) {
-    ws.close(1000, 'Authentication required');
+  if (!accessToken) {
+    ws.close(1000, 'Login required');
     return null;
   }
   
   try {
     const secret: string = process.env.JWT_SECRET as string;
-    const decoded: CustomJwtPayload = jwt.verify(token, secret) as CustomJwtPayload;
+    const decoded: CustomJwtPayload = jwt.verify(accessToken, secret) as CustomJwtPayload;
     
-    if ((await cache.isAccessTokenBlacklisted(decoded.id, token)) || decoded.reset) {
-      ws.close(1000, 'Authentication required');
+    if ((await cache.isAccessTokenBlacklisted(decoded.id, accessToken)) || decoded.reset) {
+      ws.close(1000, 'Login required');
       return null;
     }
     
@@ -55,7 +55,7 @@ export default async function wsAuthMiddleware(
     return user;
   } catch (error) {
     console.error('WebSocket authentication error:', error);
-    ws.close(1000, 'Authentication failed');
+    ws.close(1000, 'Websocket authentication failed');
     return null;
   }
 }
